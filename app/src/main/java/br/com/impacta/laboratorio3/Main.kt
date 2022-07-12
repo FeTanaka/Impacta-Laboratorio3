fc14c02d07bba3382ce3351c8265a46896e628a8
curso <curso@curso.com>
2022-07-11 23:35:56 -0300

    fix

--- app/src/main/java/br/com/impacta/laboratorio3/Main.kt
+++ app/src/main/java/br/com/impacta/laboratorio3/Main.kt
@@ -2,6 +2,6 @@ package br.com.impacta.laboratorio3
 
 fun main() {
     var idade = 33
-    var desconto = if (idade >= 50) 30 else if (idade >= 25 && idade < 50) 15 else if (idade < 25) 15 else 0
+    var desconto = if (idade >= 50) 30 else if (idade >= 25 && idade < 50) 15 else 5
     println ("O valor do desconto é $desconto%")
 }
