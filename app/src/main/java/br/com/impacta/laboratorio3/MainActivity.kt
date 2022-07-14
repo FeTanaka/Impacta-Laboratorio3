bf4aca104fa6bdb0388d898de84d3f10305fb977
curso <curso@curso.com>
2022-07-14 20:47:34 -0300

    resolução Android 2

--- app/src/main/java/br/com/impacta/laboratorio3/MainActivity.kt
+++ app/src/main/java/br/com/impacta/laboratorio3/MainActivity.kt
@@ -5,31 +5,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import br.com.impacta.laboratorio3.databinding.ActivityMainBinding
+import br.com.impacta.laboratorio3.databinding.TesteBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var editText: EditText? = null
-    var button: Button? = null
-    var textView: TextView? = null
+    var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        var root = binding.root
+        setContentView(root)
 
-        editText = findViewById(R.id.editTextNumber)
-        button = findViewById(R.id.button)
-        textView = findViewById(R.id.textView)
-
-        button?.setOnClickListener {
-            if (editText?.text.toString() == "") {
-                textView?.text = "Por favor digite sua idade."
+        binding.button.setOnClickListener {
+            if (binding.editTextNumber.text.toString() == "") {
+                binding.textView.text = "Por favor digite sua idade."
             } else {
-                var idade = editText?.text.toString().toInt()
+                var idade = binding.editTextNumber.text.toString().toInt()
                 var desconto = if (idade > 50) 30
                 else if (idade in 25..50)  15
                 else  5
 
-                textView?.text = "A quantidade de desconto é ${desconto}%"
+                binding.textView.text = "A quantidade de desconto é ${desconto}%"
             }
         }
     }
